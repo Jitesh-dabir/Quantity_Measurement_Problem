40eac99a5f65deaef1087e726b541818c8f42042
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-09 19:29:47 +0530

    Added: Method to check equality

--- src/main/java/QuantityMeasurement.java
+++ src/main/java/QuantityMeasurement.java
@@ -1,4 +1,26 @@
+import java.util.Objects;
+
 public class QuantityMeasurement {
+
+    private final double feet;
+
+    public QuantityMeasurement(double feet) {
+        this.feet = feet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return Double.compare(that.feet, feet) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(feet);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to quantity measurement system");
     }
