643b4ccdec9788b608e5e76f475b184cba8d0311
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-09 19:28:51 +0530

    Added: Test case given two feets and check equality

---
+++ src/test/java/QuantityMeasurementTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuantityMeasurementTest {
+
+    @Test
+    public void given0FeetAnd0feet_WhenEqual_ShouldReturnTrue() {
+        QuantityMeasurement firstFeet = new QuantityMeasurement(0.0);
+        QuantityMeasurement secondFeet = new QuantityMeasurement(0.0);
+        boolean result = firstFeet.equals(secondFeet);
+        Assert.assertEquals(true, result);
+    }
+}
