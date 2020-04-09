c7135cb5d157e77a7a8a3c11961c4bff10dce1cd
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-09 19:38:49 +0530

    Added: Test case to check equality when pass null should return false

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -10,4 +10,12 @@ public class QuantityMeasurementTest {
         boolean result = firstFeet.equals(secondFeet);
         Assert.assertEquals(true, result);
     }
+
+    @Test
+    public void given0FeetAnd0feet_WhenNullCheck_ShouldReturnResult() {
+        QuantityMeasurement firstFeet = new QuantityMeasurement(0.0);
+        QuantityMeasurement secondFeet = new QuantityMeasurement(0.0);
+        boolean result = firstFeet.equals(null);
+        Assert.assertEquals(false, result);
+    }
 }
