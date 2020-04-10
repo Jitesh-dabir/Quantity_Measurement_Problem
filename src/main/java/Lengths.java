f578221101c2c3ab5d98e265dea4e14aeeec550a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-11 00:29:47 +0530

    Added: Enum for conversion of liter to millilitre

--- src/main/java/Lengths.java
+++ src/main/java/Lengths.java
@@ -1,8 +1,9 @@
 public enum Lengths {
     FEET(1 * 12.0), INCH_TO_FEET(1 / 12.0), FEET_TO_YARD(1 / 3.0),
     INCH_TO_YARD(1 / 36.0), YARD_TO_INCH(1 * 36.0), YARD_TO_FEET(1 * 3.0),
-    INCH_TO_CM(1 * 2.54), CM_TO_INCH(1 / 2.54),INCH(1.0),
-    GALLON_TO_LITRES(1 * 3.78);
+    INCH_TO_CM(1 * 2.54), CM_TO_INCH(1 / 2.54), INCH(1.0),
+    GALLON_TO_LITRES(1 * 3.78), LITRES_TO_GALLON(1 / 3.78),
+    LITER_TO_MILLILITER(1 * 1000);
 
     public final double value;
 
