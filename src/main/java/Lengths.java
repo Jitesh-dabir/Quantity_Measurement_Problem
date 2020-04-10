fd59d1581aadc654560188582414cfb26b7064c6
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-10 15:58:44 +0530

    Added: Enum for coversion of inch to yard

--- src/main/java/Lengths.java
+++ src/main/java/Lengths.java
@@ -1,5 +1,5 @@
 public enum Lengths {
-    FEET(1 * 12.0), INCH(1 / 12.0), FEET_TO_YARD(1 / 3.0);
+    FEET(1 * 12.0), INCH(1 / 12.0), FEET_TO_YARD(1 / 3.0), INCH_TO_YARD(1 / 36.0);
 
     public final double value;
 
