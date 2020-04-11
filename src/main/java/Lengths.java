c5ca069ca86a6a0a318d285bb15585cd3d0cc31a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-11 10:39:45 +0530

    Added: Enum for conversion of tonne to kilograms

--- src/main/java/Lengths.java
+++ src/main/java/Lengths.java
@@ -4,7 +4,7 @@ public enum Lengths {
     INCH_TO_CM(1 * 2.54), CM_TO_INCH(1 / 2.54), INCH(1.0),
     GALLON_TO_LITRES(1 * 3.78), LITRES_TO_GALLON(1 / 3.78),
     LITER_TO_MILLILITER(1 * 1000.0), MILLILITER_TO_LITER(1 / 1000.0),
-    KG_TO_GRAMS(1 * 1000);
+    KG_TO_GRAMS(1 * 1000.0), TONNE_TO_KG(1 / 1000.0);
     public final double value;
 
     Lengths(double value) {
